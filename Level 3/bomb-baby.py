247e08a929f76d4b1fd68aa50190f18e2c9fcf16
Manogya Singh <manogyasinghsuryansh@gmail.com>
2024-03-01 23:18:18 +0530

    Update bomb-baby.py

--- Level 3/bomb-baby.py
+++ Level 3/bomb-baby.py
@@ -1,33 +1,9 @@
-def solution(m,f):
-    sol=(int(m),int(f))
-    
-    cgen={(1,1)}
-    cgenbutbetter=set()
-    ngen=set()
-    gens=0
+def solution(a,b):
+    a,b,c=int(a),int(b),-1
+    if a<b:a,b=b,a
+    while(b):
+        c+=a//b
+        a,b=b,a%b
+    return c if a==1 else "impossible"
 
-    while (len(cgen)):
-        #print(cgen)
-        #remove cases which are already larger than what we want
-        for i in cgen:
-            if i[0] <= sol[0] and i[1] <= sol[1]:
-                cgenbutbetter.add(i)
-        cgen=cgenbutbetter
-        cgenbutbetter=set()        
-        #check if solved
-        if sol in cgen:
-            return str(gens)
-
-        #recreation or something
-        for i in cgen:
-            ngen.add((i[0],i[1]+i[0]))
-            ngen.add((i[0]+i[1],i[1]))
-
-        #prepare for next cycle
-        cgen=ngen
-        ngen=set()
-        gens+=1
-    
-    return "impossible"
-
-print(solution(4,7))
+#print(solution(4,7))
